07273b4a140725adaa1b65e94fd3a64bcd0d36ec
khashayaramini <khashayaramini80@gmail.com>
2024-10-22 01:10:41 +0300

    tmux init

--- .init_tmux.sh
+++ .init_tmux.sh
@@ -10,7 +10,8 @@ tmux send-keys -t home:0 'c' C-m
 tmux send-keys -t home:1 'c' C-m
 tmux send-keys -t home:2 'c' C-m
 
-tmux select-window -t home:0
 tmux send-keys -t home:0 'n' C-m
+
+tmux select-window -t home:1
 
 tmux attach-session -t home
