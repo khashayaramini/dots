110fd0261689f3c5c8ffe28424da5c1636c84a2f
khashayaramini <khashayaramini80@gmail.com>
2024-10-22 01:55:22 +0300

    tmux init

--- .init_tmux.sh
+++ .init_tmux.sh
@@ -2,16 +2,16 @@
 
 tmux new-session -d -s home
 
-tmux rename-window -t home:0 'editor'
-tmux new-window -t home:1 -n 'terminal'
-tmux new-window -t home:2 -n 'misc'
+tmux rename-window -t home:1 'editor'
+tmux new-window -t home:2 -n 'terminal'
+tmux new-window -t home:3 -n 'misc'
 
-tmux send-keys -t home:0 'c' C-m
 tmux send-keys -t home:1 'c' C-m
 tmux send-keys -t home:2 'c' C-m
+tmux send-keys -t home:3 'c' C-m
 
-tmux send-keys -t home:0 'n' C-m
+tmux send-keys -t home:1 'n' C-m
 
-tmux select-window -t home:1
+tmux select-window -t home:2
 
 tmux attach-session -t home
